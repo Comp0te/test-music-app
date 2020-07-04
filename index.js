4b1c362417668402e158845764bb6f6506c236cb
Dmytro Vynnyk <dmytro.v@avocadotechnology.com>
2020-07-04 15:33:10 +0300

    Add background service

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { AppRegistry } from 'react-native';
+import * as TrackPlayer from 'react-native-track-player';
+
 import { name as appName } from './app.json';
 import { App } from './src/app';
 
 AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() => require('./service.js'));
